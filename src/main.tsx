a76799adefcff72ae019ccc6f3ef8fb025441acd
tcrow06 <tcrow0604@gmail.com>
2026-06-22 13:48:33 +0700

    add tesst

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { AppProviders } from "@/app/providers";
 import { router } from "@/app/router";
 import "@/styles.css";
-
+// Tesst
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProviders>
